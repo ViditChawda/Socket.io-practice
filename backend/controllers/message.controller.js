9d1f0f27061ed6e2f2a0ad6d88287a567e2cc210
ViditChawda <viditchawda301@gmail.com>
2024-07-01 00:42:21 +0530

    Message bugs

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -5,25 +5,30 @@ export const sendMessage = async (req, res) => {
     try {
         console.log('send message called')
         const { message } = req.body;
-        const { id: receiverId } = req.params;
-        const senderId = req.uesr._id
+        const { userId: receiverId } = req.params;
+        const senderId = req.user._id
 
         let conversation = await Conversation.findOne({
             participants: { $all: [senderId, receiverId] }
         })
-
+        console.log('consversation', conversation)
         if (!conversation) {
             conversation = await Conversation.create({
                 participants: [senderId, receiverId]
             })
+            console.log('created a conversation')
         }
-
         const newMessage = new Message({
             senderId,
             receiverId,
             message
         })
 
+        if (newMessage) {
+            conversation.messages.push(newMessage._id)
+        }
+
+        res.status(201).json(newMessage)
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" })
     }
--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -9,15 +9,16 @@ const protectRoute = async (req, res, next) => {
             return res.status(401).json({ error: "Unauthorised - No Token Provided" })
         }
         const decoded = jwt.verify(token, "DBASJDKADSA")
-
+        console.log('decoded', decoded)
         if (!decoded) {
             return res.status(401).json({ error: "Unauthorised - Invalid Token" })
         }
         const user = await User.findById(decoded.userId,).select('-password')
-
+        console.log(user)
         req.user = user
 
         next();
+        console.log('no error in the protected Route')
     } catch (error) {
         console.log('Error in protectRoute middleware: ', error.message)
         res.status(500).json({ error: "Internal server error" });
